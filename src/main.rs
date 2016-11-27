f5c2175258163646f8b0249f4f8af30673990969
Matt Mullins <mmullins@mmlx.us>
2016-11-26 20:40:24 -0800

    main: print a board using Unicode line drawing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 mod board;
 
 fn main() {
-    println!("Hello, world!");
+    let width = 78;
+	let height = 20;
+
+	let board = board::Board::new(width, height);
+
+	print!("┌");
+	for _ in 0..width {
+		print!("─");
+	}
+	print!("┐\n");
+
+	use board::Cell::*;
+
+	for row in 0..height {
+		print!("│");
+		for col in 0..width {
+			let c = match board.at(col, row).unwrap() {
+				Empty => " ",
+				Snake(_) => "\u{2588}",
+				Target => "\u{2592}",
+			};
+			print!("{}", c);
+		}
+		print!("│\n");
+	}
+
+	print!("└");
+	for _ in 0..width {
+		print!("─");
+	}
+	print!("┘\n");
 }
