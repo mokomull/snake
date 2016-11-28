f7eb78086d5ccad30c8bec0cae0efd3700364386
Matt Mullins <mmullins@mmlx.us>
2016-11-27 18:00:32 -0800

    Run the game loop when there's input.
    I added a test in main.rs because "match &foo" actually does compare the value
    of foo.  I was getting weird match failures in set_direction, but that turned
    out to be caused by the terminal sending [ESC] O A in one context, but
    [ESC] [ A when Rust was running.
    
    It's almost playable now.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod board;
-
-use std::time::Duration;
-use std::thread::sleep;
+extern crate termios;
 
 const WIDTH: usize = 78;
 const HEIGHT: usize  = 20;
@@ -41,13 +39,62 @@ fn dump(game: &board::Game) {
 	print!("┘\n");
 }
 
+fn set_direction(game: &mut board::Game) {
+	use std::io::Read;
+	use board::Direction::*;
+
+	let mut stdin = std::io::stdin();
+	let mut buf = [0; 3];
+
+	match stdin.read(&mut buf) {
+		Ok(3) => {
+			match &buf {
+				// For some reason, these are [ESC] O A (etc.) in zsh, but Rust
+				// is receiving [ESC] [ A (etc.).
+				b"\x1b[A" => game.set_direction(Up),
+				b"\x1b[B" => game.set_direction(Down),
+				b"\x1b[C" => game.set_direction(Right),
+				b"\x1b[D" => game.set_direction(Left),
+				_ => println!("Not an arrow key: {:?}", buf),
+			}
+		},
+		Ok(_) => println!("Got too few bytes."),
+		Err(e) => panic!("{}", e),
+	}
+}
+
 fn main() {
+	use termios::{Termios, ICANON, ECHO, TCSANOW, tcsetattr};
+
 	let mut game = board::Game::new(WIDTH, HEIGHT);
-	let frame_time = Duration::from_millis(500);
+
+	let mut termios = Termios::from_fd(0).unwrap();
+	termios.c_lflag &= !(ICANON | ECHO);
+	match tcsetattr(0, TCSANOW, &termios) {
+		Ok(_) => (),
+		Err(x) => panic!("tcsetattr: {}", x),
+	}
 
 	loop {
 		dump(&game);
-		sleep(frame_time);
+		set_direction(&mut game);
 		game.tick();
+	}
+}
+
+#[test]
+fn make_sure_match_ref_equality_works_like_i_think() {
+	let mut buf = [0 as u8; 3];
+	let other = b"\x1bOA";
+
+	buf[0] = b'\x1b';
+	buf[1] = b'O';
+	buf[2] = b'A';
+
+	assert_eq!(&buf, other);
+
+	match &buf {
+		b"\x1bOA" => (),
+		_ => panic!("nope"),
 	}
 }
