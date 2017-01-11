01297e7e998c0562d1190944e5787261aff19025
Matt Mullins <mmullins@mmlx.us>
2017-01-10 23:34:02 -0800

    Futures!  Slightly insane, still some TODO.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 mod board;
+extern crate byteorder;
 extern crate x11_client;
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_uds;
 
-use std::os::unix::net::UnixStream;
 use std::io::prelude::*;
+use std::io::Cursor;
 
 use x11_client::*;
+use byteorder::{ByteOrder, BigEndian};
+use futures::{Future, BoxFuture};
+use tokio_uds::UnixStream;
+use tokio_core::io::{read_exact, write_all};
 
 const WIDTH: usize = 64;
 const HEIGHT: usize  = 64;
@@ -35,88 +43,99 @@ fn main() {
 
 	let interval = std::time::Duration::from_millis(100);
 
-    let mut socket = UnixStream::connect("/tmp/.X11-unix/X0").unwrap();
+	let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+
+    let socket = UnixStream::connect("/tmp/.X11-unix/X0", &handle).unwrap();
     let client_init: Vec<_> = ClientInit::new().into();
-    socket.write(&client_init).unwrap();
+    let f = write_all(socket, &client_init).and_then(|(socket, _)| {
+        let server_init_prefix = vec![0 as u8; 8];
 
-    let server_init = ServerInit::from_stream(&mut socket).unwrap();
-    let window = server_init.resource_id_base + 1;
-    let snake_gc = window + 1;
-    let bg_gc = snake_gc + 1;
-    let target_gc = bg_gc + 1;
+        // TODO: the destructuring of the server response length really belongs
+        // in x11_client, since it depends on the byteorder that it serialized
+        // into ClientInit.
+        read_exact(socket, server_init_prefix).and_then(|(socket, mut server_init_prefix)| {
+            assert_eq!(1, server_init_prefix[0]);
+            let length = BigEndian::read_u16(&server_init_prefix[6..8]);
+            read_exact(socket, vec![0 as u8; (length * 4) as usize]).and_then(move |(socket, mut server_init_rest)| {
+                let mut server_init_data = Vec::new();
+                server_init_data.append(&mut server_init_prefix);
+                server_init_data.append(&mut server_init_rest);
 
-    socket.write(&CreateWindow::new(
-        24,
-        window,
-        server_init.roots[0].root,
-        0, 0,
-        (WIDTH * SNAKE_SIZE) as u16, (HEIGHT * SNAKE_SIZE) as u16,
-        0, // border-width
-        1, // InputOutput
-        0, // visual: CopyFromParent
-    ).as_bytes()).unwrap();
+                let server_init = ServerInit::from_stream(&mut Cursor::new(server_init_data)).unwrap();
+                futures::future::ok((socket, server_init))
+            })
+        }).and_then(|(socket, server_init)| {
+            let window = server_init.resource_id_base + 1;
+            let snake_gc = window + 1;
+            let bg_gc = snake_gc + 1;
+            let target_gc = bg_gc + 1;
 
-    socket.write(&MapWindow::new(window).as_bytes()).unwrap();
-    socket.write(&ChangeWmName::new(
-        window,
-        "Snake".into()
-    ).as_bytes()).unwrap();
+            write_all(socket, CreateWindow::new(
+                24,
+                window,
+                server_init.roots[0].root,
+                0, 0,
+                (WIDTH * SNAKE_SIZE) as u16, (HEIGHT * SNAKE_SIZE) as u16,
+                0, // border-width
+                1, // InputOutput
+                0, // visual: CopyFromParent
+            ).as_bytes()).and_then(move |(socket, _)| {
+                write_all(socket, MapWindow::new(window).as_bytes())
+            }).and_then(move |(socket, _)| {
+                write_all(socket, ChangeWmName::new(
+                    window,
+                    "Snake".into()
+                ).as_bytes())
+            }).and_then(move |(socket, _)| {
+                write_all(socket, CreateGc::new(
+                    snake_gc, window, 0x00AA00,
+                ).as_bytes())
+            }).and_then(move |(socket, _)| {
+                write_all(socket, CreateGc::new(
+                    bg_gc, window, 0xFFFFFF,
+                ).as_bytes())
+            }).and_then(move |(socket, _)| {
+                write_all(socket, CreateGc::new(
+                    target_gc, window, 0xee9922,
+                ).as_bytes())
+            }).and_then(move |(socket, _)| {
+                fn event_handler<T: Read+Write+Send+'static>(socket: T, mut game: board::Game, window: u32, snake_gc: u32, target_gc: u32, bg_gc: u32)
+                        -> BoxFuture<(), std::io::Error> {
+                    read_exact(socket, [0 as u8; 32]).and_then(move |(mut socket, result)| {
+                        let event = Event::from_bytes(&result);
 
-    socket.write(&CreateGc::new(
-        snake_gc, window, 0x00AA00,
-    ).as_bytes()).unwrap();
+                        // TODO: run this on a timer
+                        game.tick();
 
-    socket.write(&CreateGc::new(
-        bg_gc, window, 0xFFFFFF,
-    ).as_bytes()).unwrap();
-
-    socket.write(&CreateGc::new(
-        target_gc, window, 0xee9922,
-    ).as_bytes()).unwrap();
-
-    socket.set_read_timeout(Some(interval)).unwrap();
-    loop {
-        let mut buf = [0 as u8; 32];
-        let result = socket.read_exact(&mut buf);
-        match result {
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                game.tick();
-                dump(
-                    &mut socket,
-                    &game,
-                    window, snake_gc, target_gc, bg_gc
-                );
-                continue;
-            }
-            Err(e) => { // probably EOF
-                println!("Unexpected error: {:?}", e);
-                return;
-            }
-            _ => {}
-        }
-
-        let event = Event::from_bytes(&buf);
-
-        match event {
-            Event::Expose {..} => dump(
-                &mut socket, &game,
-                window, snake_gc, target_gc, bg_gc
-            ),
-            Event::KeyPress { detail: key, .. } => {
-                use board::Direction::*;
-                match key {
-                    111 => game.set_direction(Up),
-                    113 => game.set_direction(Left),
-                    114 => game.set_direction(Right),
-                    116 => game.set_direction(Down),
-                    _ => (),
+                        match event {
+                            Event::Expose {..} => dump(
+                                &mut socket, &game,
+                                window, snake_gc, target_gc, bg_gc
+                            ),
+                            Event::KeyPress { detail: key, .. } => {
+                                use board::Direction::*;
+                                match key {
+                                    111 => game.set_direction(Up),
+                                    113 => game.set_direction(Left),
+                                    114 => game.set_direction(Right),
+                                    116 => game.set_direction(Down),
+                                    _ => (),
+                                }
+                            }
+                            _ => {
+                                println!("Unhandled event: {:?}", event);
+                            }
+                        }
+                        event_handler(socket, game, window, snake_gc, target_gc, bg_gc)
+                    }).boxed()
                 }
-            }
-            _ => {
-                println!("Unhandled event: {:?}", event);
-            }
-        }
-    }
+                event_handler(socket, game, window, snake_gc, target_gc, bg_gc)
+            })
+        })
+    });
+
+    core.run(f).unwrap();
 }
 
 #[test]
