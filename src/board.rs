21e9ddd0872363e26734e4974714eace80b2a2ee
Matt Mullins <mmullins@mmlx.us>
2017-08-07 22:19:08 -0700

    rustfmt
    I added some minor changes myself to make rustfmt create the indentation I
    wanted, but the end result is definitely better anyway.

--- src/board.rs
+++ src/board.rs
@@ -7,235 +7,246 @@ extern crate rand;
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub enum Direction {
-	Up = 1,
-	Left = 2,
-	Down = 3,
-	Right = 4,
+    Up = 1,
+    Left = 2,
+    Down = 3,
+    Right = 4,
 }
 
 impl From<usize> for Direction {
-	fn from(u: usize) -> Direction {
-		match u {
-			1 => Direction::Up,
-			2 => Direction::Left,
-			3 => Direction::Down,
-			_ => Direction::Right,
-		}
-	}
+    fn from(u: usize) -> Direction {
+        match u {
+            1 => Direction::Up,
+            2 => Direction::Left,
+            3 => Direction::Down,
+            _ => Direction::Right,
+        }
+    }
 }
 
 #[derive(Clone)]
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub enum Cell {
-	Empty,
-	Target,
-	Snake(Direction),
+    Empty,
+    Target,
+    Snake(Direction),
 }
 
 struct Board {
-	// Indexed by row, then by column, to make printing to the screen easier later.
-	board: Vec<Vec<Cell>>,
+    // Indexed by row, then by column, to make printing to the screen easier later.
+    board: Vec<Vec<Cell>>,
 }
 
 use self::Cell::*;
 impl Board {
-	pub fn new(width: usize, height: usize) -> Board {
-		let mut board = Vec::<Vec<Cell>>::with_capacity(height);
+    pub fn new(width: usize, height: usize) -> Board {
+        let mut board = Vec::<Vec<Cell>>::with_capacity(height);
 
-		for _row in 0..height {
-			board.push(vec![Empty; width])
-		}
+        for _row in 0..height {
+            board.push(vec![Empty; width])
+        }
 
-		Board {
-			board: board,
-		}
-	}
+        Board { board: board }
+    }
 
-	pub fn at(&self, column: usize, row: usize) -> Option<Cell> {
-		// .clone() because for some reason it won't copy the referent, even
-		// though it's trivial.
-		self.board.get(row).and_then(|r| r.get(column)).cloned()
-	}
+    pub fn at(&self, column: usize, row: usize) -> Option<Cell> {
+        // .clone() because for some reason it won't copy the referent, even
+        // though it's trivial.
+        self.board.get(row).and_then(|r| r.get(column)).cloned()
+    }
 }
 
 pub struct Game {
-	board: Board,
-	head: (usize, usize),
-	tail: (usize, usize),
-	width: usize,
-	height: usize,
+    board: Board,
+    head: (usize, usize),
+    tail: (usize, usize),
+    width: usize,
+    height: usize,
 }
 
 impl Game {
-	pub fn new(width: usize, height: usize) -> Game {
-		let mut board = Board::new(width, height);
-		let column = width / 2;
-		let row = height / 2;
+    pub fn new(width: usize, height: usize) -> Game {
+        let mut board = Board::new(width, height);
+        let column = width / 2;
+        let row = height / 2;
 
-		// TODO: poor encapsulation of the board
-		board.board[0][0] = Target;
-		board.board[row][column] = Snake(Direction::Down);
+        // TODO: poor encapsulation of the board
+        board.board[0][0] = Target;
+        board.board[row][column] = Snake(Direction::Down);
 
-		Game {
-			board: board,
-			head: (column, row),
-			tail: (column, row),
-			width: width,
-			height: height,
-		}
-	}
+        Game {
+            board: board,
+            head: (column, row),
+            tail: (column, row),
+            width: width,
+            height: height,
+        }
+    }
 
-	pub fn get_direction(&self) -> Direction {
-		let (column, row) = self.head;
-		match self.board.at(column, row) {
-			Some(Snake(d)) => d,
-			_ => panic!("The snake doesn't have a head"),
-		}
-	}
+    pub fn get_direction(&self) -> Direction {
+        let (column, row) = self.head;
+        match self.board.at(column, row) {
+            Some(Snake(d)) => d,
+            _ => panic!("The snake doesn't have a head"),
+        }
+    }
 
-	pub fn set_direction(&mut self, d: Direction) {
-		let (column, row) = self.head;
-		// TODO: poorly encapsulated.
-		self.board.board[row][column] = Snake(d);
-	}
+    pub fn set_direction(&mut self, d: Direction) {
+        let (column, row) = self.head;
+        // TODO: poorly encapsulated.
+        self.board.board[row][column] = Snake(d);
+    }
 
-	pub fn tick(&mut self) {
-		let (column, row) = self.head;
-		match self.next(self.get_direction(), column, row) {
-			Some((new_column, new_row)) => {
-				match self.board.at(new_column, new_row) {
-					Some(Empty) => {
-						// Move forward normally - shrink the tail
-						let (column, row) = self.tail;
-						let d = match self.board.at(column, row).unwrap() {
-							Snake(d) => d,
-							_ => panic!("not a snake at the tail"),
-						};
-						// if the tail moves into a wall, the player already died
-						let (new_tcol, new_trow) = self.next(d, column, row).unwrap();
+    pub fn tick(&mut self) {
+        let (column, row) = self.head;
+        match self.next(self.get_direction(), column, row) {
+            Some((new_column, new_row)) => {
+                match self.board.at(new_column, new_row) {
+                    Some(Empty) => {
+                        // Move forward normally - shrink the tail
+                        let (column, row) = self.tail;
+                        let d = match self.board.at(column, row).unwrap() {
+                            Snake(d) => d,
+                            _ => panic!("not a snake at the tail"),
+                        };
+                        // if the tail moves into a wall, the player already died
+                        let (new_tcol, new_trow) = self.next(d, column, row).unwrap();
 
-						// TODO: encapsulation violation, again
-						self.board.board[new_row][new_column] = Snake(self.get_direction());
-						self.board.board[row][column] = Empty;
-						self.head = (new_column, new_row);
-						self.tail = (new_tcol, new_trow);
-					},
-					Some(Target) => {
-						// You ate the target - leave the tail and grow
-						// TODO: board encapsulation.  Yet again.
-						self.board.board[new_row][new_column] = Snake(self.get_direction());
-						self.head = (new_column, new_row);
+                        // TODO: encapsulation violation, again
+                        self.board.board[new_row][new_column] = Snake(self.get_direction());
+                        self.board.board[row][column] = Empty;
+                        self.head = (new_column, new_row);
+                        self.tail = (new_tcol, new_trow);
+                    }
+                    Some(Target) => {
+                        // You ate the target - leave the tail and grow
+                        // TODO: board encapsulation.  Yet again.
+                        self.board.board[new_row][new_column] = Snake(self.get_direction());
+                        self.head = (new_column, new_row);
 
-						// Randomly generate a new target
-						let mut rng = rand::thread_rng();
-						let mut col_range = rand::distributions::Range::new(0, self.width);
-						let mut row_range = rand::distributions::Range::new(0, self.height);
-						use board::rand::distributions::Sample;
-						loop {
-							let (new_col, new_row) = (col_range.sample(&mut rng), row_range.sample(&mut rng));
+                        // Randomly generate a new target
+                        let mut rng = rand::thread_rng();
+                        let mut col_range = rand::distributions::Range::new(0, self.width);
+                        let mut row_range = rand::distributions::Range::new(0, self.height);
+                        use board::rand::distributions::Sample;
+                        loop {
+                            let (new_col, new_row) =
+                                (col_range.sample(&mut rng), row_range.sample(&mut rng));
 
-							match self.board.at(new_col, new_row) {
-								Some(Empty) | Some(Target) => {
-									self.board.board[new_row][new_col] = Target;
-									break;
-								},
-								_ => (), // keep going
-							}
-						}
-					},
-					Some(Snake(_)) => panic!("You died."),
-					None => panic!("You died."),
-				}
-			}
-			None => panic!("You died."),
-		}
-	}
+                            match self.board.at(new_col, new_row) {
+                                Some(Empty) | Some(Target) => {
+                                    self.board.board[new_row][new_col] = Target;
+                                    break;
+                                }
+                                _ => (), // keep going
+                            }
+                        }
+                    }
+                    Some(Snake(_)) => panic!("You died."),
+                    None => panic!("You died."),
+                }
+            }
+            None => panic!("You died."),
+        }
+    }
 
-	fn next(&self, d: Direction, column: usize, row: usize) -> Option<(usize, usize)> {
-		use self::Direction::*;
-		match d {
-			Left => if column > 0 { Some((column-1, row)) } else { None },
-			Right => self.board.at(column+1, row).map(|_| (column+1, row)),
-			Up => if row > 0 { Some((column, row-1)) } else { None },
-			Down => self.board.at(column, row+1).map(|_| (column, row+1)),
-		}
-	}
+    fn next(&self, d: Direction, column: usize, row: usize) -> Option<(usize, usize)> {
+        use self::Direction::*;
+        match d {
+            Left => {
+                if column > 0 {
+                    Some((column - 1, row))
+                } else {
+                    None
+                }
+            }
+            Right => self.board.at(column + 1, row).map(|_| (column + 1, row)),
+            Up => {
+                if row > 0 {
+                    Some((column, row - 1))
+                } else {
+                    None
+                }
+            }
+            Down => self.board.at(column, row + 1).map(|_| (column, row + 1)),
+        }
+    }
 
-	pub fn at(&self, column: usize, row: usize) -> Option<Cell> {
-		self.board.at(column, row)
-	}
+    pub fn at(&self, column: usize, row: usize) -> Option<Cell> {
+        self.board.at(column, row)
+    }
 }
 
 #[cfg(test)]
 mod test {
-	use super::*;
-	use super::Cell::*;
+    use super::*;
+    use super::Cell::*;
 
-	#[test]
-	fn i_have_no_idea_what_im_doing() {
-		let board = super::Board::new(80, 24);
+    #[test]
+    fn i_have_no_idea_what_im_doing() {
+        let board = super::Board::new(80, 24);
 
-		assert_eq!(Some(Empty), board.at(12, 12));
-		assert_eq!(None, board.at(80, 0));
-		assert_eq!(None, board.at(0, 24));
-		assert_eq!(Some(Empty), board.at(79, 0));
-		assert_eq!(Some(Empty), board.at(0, 23));
-	}
+        assert_eq!(Some(Empty), board.at(12, 12));
+        assert_eq!(None, board.at(80, 0));
+        assert_eq!(None, board.at(0, 24));
+        assert_eq!(Some(Empty), board.at(79, 0));
+        assert_eq!(Some(Empty), board.at(0, 23));
+    }
 
-	#[test]
-	fn game_tick() {
-		use super::Direction::*;
+    #[test]
+    fn game_tick() {
+        use super::Direction::*;
 
-		let mut game = Game::new(80, 24);
-		game.set_direction(Up);
+        let mut game = Game::new(80, 24);
+        game.set_direction(Up);
 
-		let (column, row) = game.head;
-		assert_eq!(Some(Snake(Up)), game.board.at(column, row));
+        let (column, row) = game.head;
+        assert_eq!(Some(Snake(Up)), game.board.at(column, row));
 
-		game.tick();
-		assert_eq!((column, row-1), game.head);
-		assert_eq!((column, row-1), game.tail);
+        game.tick();
+        assert_eq!((column, row - 1), game.head);
+        assert_eq!((column, row - 1), game.tail);
 
-		let (column, row) = game.head;
-		game.set_direction(Left);
-		game.tick();
-		assert_eq!((column-1, row), game.head);
-		assert_eq!(game.head, game.tail);
+        let (column, row) = game.head;
+        game.set_direction(Left);
+        game.tick();
+        assert_eq!((column - 1, row), game.head);
+        assert_eq!(game.head, game.tail);
 
-		let (column, row) = game.head;
-		game.set_direction(Right);
-		game.tick();
-		assert_eq!((column+1, row), game.head);
-		assert_eq!(game.head, game.tail);
+        let (column, row) = game.head;
+        game.set_direction(Right);
+        game.tick();
+        assert_eq!((column + 1, row), game.head);
+        assert_eq!(game.head, game.tail);
 
-		let (column, row) = game.head;
-		game.set_direction(Down);
-		game.tick();
-		assert_eq!((column, row+1), game.head);
-		assert_eq!(game.head, game.tail);
-	}
+        let (column, row) = game.head;
+        game.set_direction(Down);
+        game.tick();
+        assert_eq!((column, row + 1), game.head);
+        assert_eq!(game.head, game.tail);
+    }
 
-	#[test]
-	fn target() {
-		use super::Direction::*;
+    #[test]
+    fn target() {
+        use super::Direction::*;
 
-		let mut game = Game::new(80, 24);
-		game.set_direction(Up);
+        let mut game = Game::new(80, 24);
+        game.set_direction(Up);
 
-		for row in 0..24 {
-			for col in 0..80 {
-				match game.board.at(col, row) {
-					Some(Empty) => game.board.board[row][col] = Target,
-					_ => (),
-				}
-			}
-		}
+        for row in 0..24 {
+            for col in 0..80 {
+                match game.board.at(col, row) {
+                    Some(Empty) => game.board.board[row][col] = Target,
+                    _ => (),
+                }
+            }
+        }
 
-		let (column, row) = game.head;
-		let orig_tail = game.tail;
-		game.tick();
-		assert_eq!((column, row-1), game.head);
-		assert_eq!(orig_tail, game.tail);
-	}
+        let (column, row) = game.head;
+        let orig_tail = game.tail;
+        game.tick();
+        assert_eq!((column, row - 1), game.head);
+        assert_eq!(orig_tail, game.tail);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -19,36 +19,45 @@ use tokio_io::AsyncRead; // for .split()
 use tokio_io::io::{read_exact, write_all};
 
 const WIDTH: usize = 15;
-const HEIGHT: usize  = 15;
+const HEIGHT: usize = 15;
 const SNAKE_SIZE: usize = 16;
 
-fn dump<T: Write>(stream: &mut T, game: &board::Game,
-        window: u32, snake_gc: u32, target_gc: u32, bg_gc: u32) {
-	for row in 0..HEIGHT {
-		for col in 0..WIDTH {
+fn dump<T: Write>(
+    stream: &mut T,
+    game: &board::Game,
+    window: u32,
+    snake_gc: u32,
+    target_gc: u32,
+    bg_gc: u32,
+) {
+    for row in 0..HEIGHT {
+        for col in 0..WIDTH {
             let gc = match game.at(col, row).unwrap() {
                 board::Cell::Empty => bg_gc,
                 board::Cell::Target => target_gc,
                 board::Cell::Snake(_) => snake_gc,
             };
-            stream.write_all(&PolyFillRectangle::new(
-                window,
-                gc,
-                (col * SNAKE_SIZE) as i16,
-                (row * SNAKE_SIZE) as i16,
-                SNAKE_SIZE as u16, SNAKE_SIZE as u16
-            ).as_bytes()).unwrap();
-		}
-	}
+            stream
+                .write_all(&PolyFillRectangle::new(
+                    window,
+                    gc,
+                    (col * SNAKE_SIZE) as i16,
+                    (row * SNAKE_SIZE) as i16,
+                    SNAKE_SIZE as u16,
+                    SNAKE_SIZE as u16,
+                ).as_bytes())
+                .unwrap();
+        }
+    }
 }
 
 fn main() {
-	let mut game = board::Game::new(WIDTH, HEIGHT);
+    let mut game = board::Game::new(WIDTH, HEIGHT);
 
-	let mut core = tokio_core::reactor::Core::new().unwrap();
+    let mut core = tokio_core::reactor::Core::new().unwrap();
     let handle = core.handle();
 
-	let interval = Interval::new(std::time::Duration::from_millis(100), &handle).unwrap();
+    let interval = Interval::new(std::time::Duration::from_millis(100), &handle).unwrap();
 
     let socket = UnixStream::connect("/tmp/.X11-unix/X0", &handle).unwrap();
     let client_init: Vec<_> = ClientInit::new().into();
@@ -58,103 +67,117 @@ fn main() {
         // TODO: the destructuring of the server response length really belongs
         // in x11_client, since it depends on the byteorder that it serialized
         // into ClientInit.
-        read_exact(socket, server_init_prefix).and_then(|(socket, mut server_init_prefix)| {
-            assert_eq!(1, server_init_prefix[0]);
-            let length = BigEndian::read_u16(&server_init_prefix[6..8]);
-            read_exact(socket, vec![0 as u8; (length * 4) as usize]).and_then(move |(socket, mut server_init_rest)| {
-                let mut server_init_data = Vec::new();
-                server_init_data.append(&mut server_init_prefix);
-                server_init_data.append(&mut server_init_rest);
+        read_exact(socket, server_init_prefix)
+            .and_then(|(socket, mut server_init_prefix)| {
+                assert_eq!(1, server_init_prefix[0]);
+                let length = BigEndian::read_u16(&server_init_prefix[6..8]);
+                read_exact(socket, vec![0 as u8; (length * 4) as usize])
+                    .and_then(move |(socket, mut server_init_rest)| {
+                        let mut server_init_data = Vec::new();
+                        server_init_data.append(&mut server_init_prefix);
+                        server_init_data.append(&mut server_init_rest);
 
-                let server_init = ServerInit::from_stream(&mut Cursor::new(server_init_data)).unwrap();
-                futures::future::ok((socket, server_init))
+                        let server_init =
+                            ServerInit::from_stream(&mut Cursor::new(server_init_data)).unwrap();
+                        futures::future::ok((socket, server_init))
+                    })
             })
-        }).and_then(|(socket, server_init)| {
-            let window = server_init.resource_id_base + 1;
-            let snake_gc = window + 1;
-            let bg_gc = snake_gc + 1;
-            let target_gc = bg_gc + 1;
+            .and_then(|(socket, server_init)| {
+                let window = server_init.resource_id_base + 1;
+                let snake_gc = window + 1;
+                let bg_gc = snake_gc + 1;
+                let target_gc = bg_gc + 1;
 
-            write_all(socket, CreateWindow::new(
-                24,
-                window,
-                server_init.roots[0].root,
-                0, 0,
-                (WIDTH * SNAKE_SIZE) as u16, (HEIGHT * SNAKE_SIZE) as u16,
-                0, // border-width
-                1, // InputOutput
-                0, // visual: CopyFromParent
-            ).as_bytes()).and_then(move |(socket, _)| {
-                write_all(socket, MapWindow::new(window).as_bytes())
-            }).and_then(move |(socket, _)| {
-                write_all(socket, ChangeWmName::new(
+                let create_window = CreateWindow::new(
+                    24,
                     window,
-                    "Snake".into()
-                ).as_bytes())
-            }).and_then(move |(socket, _)| {
-                write_all(socket, CreateGc::new(
-                    snake_gc, window, 0x00AA00,
-                ).as_bytes())
-            }).and_then(move |(socket, _)| {
-                write_all(socket, CreateGc::new(
-                    bg_gc, window, 0xFFFFFF,
-                ).as_bytes())
-            }).and_then(move |(socket, _)| {
-                write_all(socket, CreateGc::new(
-                    target_gc, window, 0xee9922,
-                ).as_bytes())
-            }).and_then(move |(socket, _)| {
-                enum Tick {
-                    X11Event(Event),
-                    TimerFired,
-                }
+                    server_init.roots[0].root,
+                    0,
+                    0,
+                    (WIDTH * SNAKE_SIZE) as u16,
+                    (HEIGHT * SNAKE_SIZE) as u16,
+                    0, // border-width
+                    1, // InputOutput
+                    0, // visual: CopyFromParent
+                ).as_bytes();
 
-                let (read, mut write) = socket.split();
+                write_all(socket, create_window)
+                    .and_then(move |(socket, _)| {
+                        write_all(socket, MapWindow::new(window).as_bytes())
+                    })
+                    .and_then(move |(socket, _)| {
+                        write_all(socket, ChangeWmName::new(window, "Snake".into()).as_bytes())
+                    })
+                    .and_then(move |(socket, _)| {
+                        write_all(socket, CreateGc::new(snake_gc, window, 0x00AA00).as_bytes())
+                    })
+                    .and_then(move |(socket, _)| {
+                        write_all(socket, CreateGc::new(bg_gc, window, 0xFFFFFF).as_bytes())
+                    })
+                    .and_then(move |(socket, _)| {
+                        write_all(
+                            socket,
+                            CreateGc::new(target_gc, window, 0xee9922).as_bytes(),
+                        )
+                    })
+                    .and_then(move |(socket, _)| {
+                        enum Tick {
+                            X11Event(Event),
+                            TimerFired,
+                        }
 
-                let x11 = unfold(read, |read| {
-                    let f = read_exact(read, [0 as u8; 32]).map(|(socket, result)|
-                        (Tick::X11Event(Event::from_bytes(&result)), socket)
-                    );
-                    Some(f)
-                });
-                let timer = interval.map(|()| Tick::TimerFired);
+                        let (read, mut write) = socket.split();
 
-                let read_or_tick = x11.select(timer);
+                        let x11 = unfold(read, |read| {
+                            let f = read_exact(read, [0 as u8; 32]).map(|(socket, result)| {
+                                (Tick::X11Event(Event::from_bytes(&result)), socket)
+                            });
+                            Some(f)
+                        });
+                        let timer = interval.map(|()| Tick::TimerFired);
 
-                read_or_tick.for_each(move |tick| {
-                    match tick {
-                        Tick::X11Event(event) => {
-                            println!("x11");
-                            match event {
-                                Event::Expose {..} => dump(
-                                    &mut write, &game,
-                                    window, snake_gc, target_gc, bg_gc
-                                ),
-                                Event::KeyPress { detail: key, .. } => {
-                                    use board::Direction::*;
-                                    match key {
-                                        111 => game.set_direction(Up),
-                                        113 => game.set_direction(Left),
-                                        114 => game.set_direction(Right),
-                                        116 => game.set_direction(Down),
-                                        _ => (),
-                                    }
+                        let read_or_tick = x11.select(timer);
+
+                        read_or_tick.for_each(move |tick| {
+                            match tick {
+                                Tick::X11Event(event) => {
+                                    println!("x11");
+                                    match event {
+                                        Event::Expose { .. } => {
+                                            dump(
+                                                &mut write,
+                                                &game,
+                                                window,
+                                                snake_gc,
+                                                target_gc,
+                                                bg_gc,
+                                            )
+                                        }
+                                        Event::KeyPress { detail: key, .. } => {
+                                            use board::Direction::*;
+                                            match key {
+                                                111 => game.set_direction(Up),
+                                                113 => game.set_direction(Left),
+                                                114 => game.set_direction(Right),
+                                                116 => game.set_direction(Down),
+                                                _ => (),
+                                            }
+                                        }
+                                        _ => {
+                                            println!("Unhandled event: {:?}", event);
+                                        }
+                                    };
                                 }
-                                _ => {
-                                    println!("Unhandled event: {:?}", event);
+                                Tick::TimerFired => {
+                                    println!("timer");
+                                    game.tick();
+                                    dump(&mut write, &game, window, snake_gc, target_gc, bg_gc);
                                 }
                             };
-                        },
-                        Tick::TimerFired => {
-                            println!("timer");
-                            game.tick();
-                            dump(&mut write, &game, window, snake_gc, target_gc, bg_gc);
-                        },
-                    }
-                    futures::future::ok::<_, std::io::Error>(())
-                })
+                            futures::future::ok::<_, std::io::Error>(())
+                        })
+                    })
             })
-        })
     });
 
     core.run(f).unwrap();
@@ -162,17 +185,17 @@ fn main() {
 
 #[test]
 fn make_sure_match_ref_equality_works_like_i_think() {
-	let mut buf = [0 as u8; 3];
-	let other = b"\x1bOA";
+    let mut buf = [0 as u8; 3];
+    let other = b"\x1bOA";
 
-	buf[0] = b'\x1b';
-	buf[1] = b'O';
-	buf[2] = b'A';
+    buf[0] = b'\x1b';
+    buf[1] = b'O';
+    buf[2] = b'A';
 
-	assert_eq!(&buf, other);
+    assert_eq!(&buf, other);
 
-	match &buf {
-		b"\x1bOA" => (),
-		_ => panic!("nope"),
-	}
+    match &buf {
+        b"\x1bOA" => (),
+        _ => panic!("nope"),
+    }
 }
