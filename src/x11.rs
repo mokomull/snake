4ff9995e24dfad5001a382993335525b85a739f7
Matt Mullins <mmullins@mmlx.us>
2017-08-08 22:19:53 -0700

    x11: wrap read/write to re-join the split half

--- src/x11.rs
+++ src/x11.rs
@@ -1,7 +1,8 @@
 use std::io;
+use futures::Future;
 use tokio_core::reactor::Handle;
 use tokio_uds::UnixStream;
-use tokio_io::AsyncRead;
+use tokio_io::{AsyncRead, IoFuture};
 use tokio_io::io::{ReadHalf, WriteHalf, read_exact, write_all};
 
 // TODO: un-pub
@@ -20,5 +21,37 @@ impl X11Client {
             read: split.0,
             write: split.1,
         })
+    }
+
+    // TODO: I have no idea why the compiler wants 'static here
+    fn read_exact<T>(self, buf: T) -> IoFuture<(Self, T)>
+    where
+        T: AsMut<[u8]> + Send + 'static,
+    {
+        read_exact(self.read, buf)
+            .map(|(socket, result)| {
+                (
+                    Self {
+                        read: socket,
+                        write: self.write,
+                    },
+                    result,
+                )
+            })
+            .boxed()
+    }
+
+    fn write_all<T>(self, buf: T) -> IoFuture<Self>
+    where
+        T: AsRef<[u8]> + Send + 'static
+    {
+        write_all(self.write, buf)
+            .map(|(socket, _)| {
+                Self {
+                    read: self.read,
+                    write: socket,
+                }
+            })
+            .boxed()
     }
 }
